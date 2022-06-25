252fd66ee7b961627b82a04988de06ec51b7f9c0
SURYA <SURYA@LAPTOP-58HTUG6Q.bbrouter>
2022-06-25 12:34:34 +0530

    int

---
+++ variables/src/variables/integer.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class integer {
+
+	public static void main(String[] args) 
+	{
+		int max = 21478;
+		int min = -21478;
+		short shortMax=32767;
+		short shortMin=-32768;
+		long longMax=9223372L;
+		long longMin=-9223372L;
+		byte byteMax=127;
+		byte byteMin=-128;
+		System.out.println("int"+max);
+	}
+}
