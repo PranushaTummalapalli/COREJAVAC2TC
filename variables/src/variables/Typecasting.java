164b30b1e9e0ed357cbe3a538aea2352bb71fd3c
SURYA <SURYA@LAPTOP-58HTUG6Q.bbrouter>
2022-06-25 14:59:08 +0530

    long

---
+++ variables/src/variables/Typecasting.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		int i=10;
+		long l=(long)i;
+		System.out.println(i);
+		System.out.println(l);
+
+	}
+
+}
