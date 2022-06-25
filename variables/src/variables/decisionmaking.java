773327bf0b41272c68c6e9326a0a692ac6590eee
SURYA <SURYA@LAPTOP-58HTUG6Q.bbrouter>
2022-06-25 16:19:10 +0530

    OR Operator

---
+++ variables/src/variables/decisionmaking.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class decisionmaking {
+
+	public static void main(String[] args) {
+	int x=20;int y=10;
+	if(!(x<y)||(x==y))
+	{
+		System.out.println("Condition is TRUE");
+	}
+	else
+	{
+		System.out.println("Condition is FLASE");
+	}
+	}
+
+}
