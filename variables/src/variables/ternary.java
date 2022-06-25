7fec0b86f302a19680637a105b639dadfdcb1f21
SURYA <SURYA@LAPTOP-58HTUG6Q.bbrouter>
2022-06-25 16:54:45 +0530

    ternary operator

---
+++ variables/src/variables/ternary.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class ternary {
+
+	public static void main(String[] args)
+	{
+		int x=2;
+		x=(10==x)?1:0;//condition?:TRUE:FLASE
+		System.out.println(x);
+
+	}
+
+}
