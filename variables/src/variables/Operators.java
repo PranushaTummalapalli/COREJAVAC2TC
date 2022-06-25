39e7185bfd7e6d6cfb0b63380b1a020ebe4e9e67
SURYA <SURYA@LAPTOP-58HTUG6Q.bbrouter>
2022-06-25 15:17:52 +0530

    Arithematic

---
+++ variables/src/variables/Operators.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int X=10,y=20;
+		System.out.println(X+y);
+		System.out.println(X-y);
+		System.out.println(--X);
+		System.out.println(++y);
+
+	}
+
+}
