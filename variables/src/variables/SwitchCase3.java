c9ff32953efbdd55d8ce9d98cfd82fe6a71aa0f2
SURYA <SURYA@LAPTOP-58HTUG6Q>
2022-06-26 20:53:25 +0530

    switch case(String)

---
+++ variables/src/variables/SwitchCase3.java
@@ -0,0 +1,22 @@
+package variables;
+
+public class SwitchCase3
+{
+	public static void main(String[] args)
+	{
+		  String game = "Cricket";  
+	        switch(game)
+	        {  
+	        case "Hockey":  
+	            System.out.println("Let's play Hockey");  
+	            break;  
+	        case "Cricket":  
+	            System.out.println("Let's play Cricket");  
+	            break;  
+	        case "Football":  
+	            System.out.println("Let's play Football");  
+	        }  
+	        
+	}
+
+}
