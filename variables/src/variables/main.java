705f871d7da2036d0040f1a088aeaa742fbb166c
SURYA <SURYA@LAPTOP-58HTUG6Q>
2022-06-26 19:15:57 +0530

    assignment operator

---
+++ variables/src/variables/main.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class main 
+{
+	public static void main(String[] args)
+	{
+		int a=7;
+		int var;
+		var=a;
+		System.out.println("var using=:"+var);
+		var +=a;
+		System.out.println("var using +=:"+var);
+		var *=a;
+		System.out.println("var using *=:"+var);
+	}
+}
