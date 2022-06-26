116dd2379e0db7984f349d279ce9abe4444c01d9
SURYA <SURYA@LAPTOP-58HTUG6Q>
2022-06-26 19:49:06 +0530

    if...else statement

---
+++ variables/src/variables/IfElse.java
@@ -0,0 +1,31 @@
+package variables;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		int x=10;
+		if (x>=10)
+		{
+			System.out.println("x is greater than 10");
+		}
+		else
+		{
+			System.out.println("x is smaller than 10 ");
+		}
+		if (x==10)
+		{
+			System.out.println("x is equals to 10");
+		}
+		else if(x>10)
+		{
+			System.out.println("x is greaterthan 10");
+		}
+		else
+		{
+			System.out.println("x is smaller than 10");
+		}
+			
+
+	}
+
+}
