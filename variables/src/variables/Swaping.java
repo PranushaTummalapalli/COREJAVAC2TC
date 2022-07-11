027ac7f1234c068b167681e8b4947acd72110201
SURYA <SURYA@LAPTOP-58HTUG6Q>
2022-07-11 12:42:01 +0530

    swapping

---
+++ variables/src/variables/Swaping.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Swaping {
+
+	public static void main(String[] args) {
+		 int a=10,b=20;
+		 int c=a;
+		     a=b;
+		     b=c;
+		     System.out.println("swaping of two numbers is:");
+		     System.out.println("a="+a);
+		     System.out.println("b="+b);	     
+
+	}
+
+}
